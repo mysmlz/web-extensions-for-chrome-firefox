57a60149a8f9e31ab8eb2b921677421329f70389
木易什么来着 <a2472678187@163.com>
2024-09-03 12:24:36 +0800

    mbi 1.0.1

--- mdn-bcd-ie/scripts/content.ts
+++ mdn-bcd-ie/scripts/content.ts
@@ -8,14 +8,25 @@ let it: number = setInterval(() => {
         const $theadTrs: HTMLCollectionOf<HTMLTableRowElement> = $table.getElementsByTagName('thead')[0].getElementsByTagName('tr');
         const $titleTr: HTMLTableRowElement = $theadTrs[0];
         const $subTitleTr: HTMLTableRowElement = $theadTrs[1];
-        let titleNode: HTMLTableCellElement = document.createElement('th');
-        titleNode.className = 'bc-platform bc-platform-ie';
-        titleNode.title = 'ie(Powered by MDN-BCD-IE and MDN bc data.)';
-        titleNode.innerHTML = title;
+        let titleNode: HTMLTableCellElement = simpleCreateElement('th', {
+            className: 'bc-platform bc-platform-ie',
+            title: 'ie(Powered by MDN-BCD-IE and MDN bc data.)'
+        });
+        // titleNode.innerHTML = title;
+        const titleDatas: Array<SimpleElement> = [{className: 'icon icon-ie'}, {
+            className: 'visually-hidden',
+            textContent: 'ie'
+        }, {className: 'icon icon-note-warning'}, {className: 'visually-hidden', textContent: 'ie'}];
+        titleDatas.forEach(data => titleNode.appendChild(simpleCreateElement('span', data)));
         $titleTr.appendChild(titleNode);
-        let subTitleNode: HTMLTableCellElement = document.createElement('th');
-        subTitleNode.className = 'bc-browser bc-browser-ie';
-        subTitleNode.innerHTML = subTitle;
+        let subTitleNode: HTMLTableCellElement = simpleCreateElement('th', {className: 'bc-browser bc-browser-ie'});
+        // subTitleNode.className = 'bc-browser bc-browser-ie';
+        const subData: Array<SimpleElement> = [{
+            className: 'bc-head-txt-label bc-head-icon-ie',
+            textContent: 'Internet Explorer'
+        }, {className: 'bc-head-icon-symbol icon icon-ie'}];
+        subData.forEach(data => subTitleNode.appendChild(simpleCreateElement('div', data)));
+        // subTitleNode.innerHTML = subTitle;
         $subTitleTr.appendChild(subTitleNode);
 
         let xhr: XMLHttpRequest = new XMLHttpRequest();
@@ -39,28 +50,92 @@ let it: number = setInterval(() => {
                     if (_count === 1) {
                         const support: Support = (<CompatInner>bcd.data).__compat.support['ie'][0];
                         const $tbodyTr: HTMLTableRowElement = $table.getElementsByTagName('tbody')[0].getElementsByTagName('tr')[0];
-                        let contentNode: HTMLTableCellElement = document.createElement('td');
-                        contentNode.className = 'bc-support bc-browser-ie bc-supports-yes bc-has-history';
+                        let contentNode: HTMLTableCellElement = simpleCreateElement('td', {className: 'bc-support bc-browser-ie bc-supports-yes bc-has-history'});
                         let clazz: string = 'bc-level-yes icon icon-yes';
                         let supportLevel: string = 'Full support';
-                        let supportVersion: string = '';
-                        let icons: string = '';
+                        // let supportVersion: string = '';
+                        // let icons: string = '';
+                        let $supportVersion: HTMLSpanElement = document.createElement('span');
+                        let $icons: HTMLDivElement = document.createElement('div');
                         if (support.partial_implementation) {
                             clazz = 'bc-level-partial icon icon-partial';
                             supportLevel = 'Partial support';
-                            supportVersion = `<span class="bc-version-label" title="Released ${support.release_date}">${support.version_added}</span>`;
-                            icons = `<div class="bc-icons"><abbr class="only-icon" title="${support.notes}"><span>more</span><i class="icon icon-more"></i></abbr></div>`;
+                            // supportVersion = `<span class="bc-version-label" title="Released ${support.release_date}">${support.version_added}</span>`;
+                            const _supportVersionData: SimpleElement = {
+                                className: 'bc-version-label',
+                                title: `Released ${support.release_date}`,
+                                textContent: `${support.version_added}`
+                            }
+                            setElementOptions($supportVersion, _supportVersionData);
+                            // icons = `<div class="bc-icons"><abbr class="only-icon" title="${support.notes}"><span>more</span><i class="icon icon-more"></i></abbr></div>`;
+                            setElementOptions($icons, {className: 'bc-icons'});
+                            let $abbr: HTMLElement = simpleCreateElement('abbr', {
+                                className: 'only-icon',
+                                title: `${support.notes}`
+                            });
+                            let $span: HTMLSpanElement = simpleCreateElement('span', {textContent: 'more'});
+                            let $i: HTMLElement = simpleCreateElement('i', {className: 'icon icon-more'});
+                            $abbr.appendChild($span);
+                            $abbr.appendChild($i);
+                            $icons.appendChild($abbr);
                         }
                         if (support.version_added === false) {
                             clazz = 'bc-level-no icon icon-no';
                             supportLevel = 'No support';
-                            supportVersion = `<span class="bc-version-label" title="${supportLevel}"></span>`;
+                            // supportVersion = `<span class="bc-version-label" title="${supportLevel}"></span>`;
+                            $supportVersion.title = `${supportLevel}`;
                         } else {
-                            supportVersion = `<span class="bc-version-label" title="Released ${support.release_date}">${support.version_added}</span>`;
+                            // supportVersion = `<span class="bc-version-label" title="Released ${support.release_date}">${support.version_added}</span>`;
+                            const _supportVersionData: SimpleElement = {
+                                className: 'bc-version-label',
+                                title: `Released ${support.release_date}`,
+                                textContent: `${support.version_added}`
+                            }
+                            setElementOptions($supportVersion, _supportVersionData);
                         }
-                        let abbr: string = `<abbr class="${clazz}" title="${supportLevel}"><span class="bc-support-level">${supportLevel}</span></abbr>`;
-                        const content: string = `<button type="button" title="Toggle history"><div class="bcd-cell-text-wrapper"><div class="bcd-cell-icons"><span class="icon-wrap">${abbr}</span></div><div class="bcd-cell-text-copy"><span class="bc-browser-name">Internet Explorer</span>${supportVersion}</div>${icons}</div></button>`;
-                        contentNode.innerHTML = content;
+                        // let abbr: string = `<abbr class="${clazz}" title="${supportLevel}"><span class="bc-support-level">${supportLevel}</span></abbr>`;
+                        let _$abbr: HTMLElement = document.createElement('abbr');
+                        const _abbrData: SimpleElement = {
+                            className: `${clazz}`,
+                            title: `${supportLevel}`
+                        }
+                        setElementOptions(_$abbr, _abbrData);
+                        const bcSupportLevelData: SimpleElement = {
+                            className: 'bc-support-level',
+                            textContent: `${supportLevel}`
+                        }
+                        let $bcSupportLevel: HTMLSpanElement = simpleCreateElement('span', bcSupportLevelData);
+                        _$abbr.appendChild($bcSupportLevel);
+                        let $btn: HTMLButtonElement = simpleCreateElement('button', {title: 'Toggle history'});
+                        $btn.type = 'button';
+                        let $bcdCellTextWrapper: HTMLDivElement = simpleCreateElement('div', {className: 'bcd-cell-text-wrapper'});
+                        let $bcdCellIcons: HTMLDivElement = simpleCreateElement('div', {className: 'bcd-cell-icons'});
+                        let $iconWrap: HTMLSpanElement = simpleCreateElement('span', {className: 'icon-wrap'});
+                        $iconWrap.appendChild(_$abbr);
+                        $bcdCellIcons.appendChild($iconWrap);
+                        $bcdCellTextWrapper.appendChild($bcdCellIcons);
+                        let $bcdCellTextCopy: HTMLDivElement = simpleCreateElement('div', {className: 'bcd-cell-text-copy'});
+                        let $bcBrowserName: HTMLSpanElement = simpleCreateElement('span', {
+                            className: 'bc-browser-name',
+                            textContent: 'Internet Explorer'
+                        });
+                        $bcdCellTextCopy.appendChild($bcBrowserName);
+                        $bcdCellTextCopy.appendChild($supportVersion);
+                        $bcdCellTextWrapper.appendChild($bcdCellTextCopy);
+                        $btn.appendChild($bcdCellTextWrapper);
+                        $btn.appendChild($icons);
+                        // const content: string = `<button type="button" title="Toggle history">
+                        //     <div class="bcd-cell-text-wrapper">
+                        //         <div class="bcd-cell-icons">
+                        //             <span class="icon-wrap">${abbr}</span>
+                        //         </div>
+                        //         <div class="bcd-cell-text-copy">
+                        //             <span class="bc-browser-name">Internet Explorer</span>${supportVersion}
+                        //         </div>${icons}
+                        //     </div>
+                        // </button>`;
+                        // contentNode.innerHTML = content;
+                        contentNode.appendChild($btn);
                         $tbodyTr.appendChild(contentNode);
                     } else {
                         for (const key in bcd.data) {
@@ -76,6 +151,33 @@ let it: number = setInterval(() => {
         xhr.send(null);
     }
 }, 1000);
+
+function simpleCreateElement<K extends keyof HTMLElementTagNameMap>(tagName: K, options?: SimpleElement): HTMLElementTagNameMap[K] {
+    let element: HTMLElementTagNameMap[K] = document.createElement(tagName);
+    setElementOptions(element, options);
+    return element;
+}
+
+function setElementOptions(element: HTMLElement, options?: SimpleElement) {
+    if (options) {
+        const {className, title, textContent}: SimpleElement = options;
+        if (className) {
+            element.className = className;
+        }
+        if (title) {
+            element.title = title;
+        }
+        if (textContent) {
+            element.textContent = textContent;
+        }
+    }
+}
+
+interface SimpleElement {
+    className?: string;
+    title?: string;
+    textContent?: string;
+}
 
 interface Releases {
     engine: string;
